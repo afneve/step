76dfeea402ce9971d9910855434eb44a04713c68
Adam Neve <afneve@gmail.com>
2017-05-31 22:22:50 -0500

    Adding files

---
+++ steps.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var iceAge = {
+    init: function(){
+        // If user hasn't authed with Fitbit, redirect to Fitbit OAuth Implicit Grant Flow
+        var fitbitAccessToken;
+
+        if (!window.location.hash) {
+            window.location.replace('https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=227H22&redirect_uri=https%3A%2F%2F15359f83.ngrok.io%2F&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight');
+        } else {
+            var fragmentQueryParameters = {};
+            window.location.hash.slice(1).replace(
+                new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+                function($0, $1, $2, $3) { fragmentQueryParameters[$1] = $3; }
+            );
+
+            fitbitAccessToken = fragmentQueryParameters.access_token;
+        }
+
+        fetch(
+            'https://api.fitbit.com/1/user/-/activities/heart/date/2016-03-19/1d/1sec/time/21:00/23:00.json',
+        {
+            headers: new Headers({
+                'Authorization': 'Bearer ' + fitbitAccessToken
+            }),
+            mode: 'cors',
+            method: 'GET'
+        }
+        ).then(processResponse)
+        .catch(function(error) {
+            console.log(error);
+        });
+    },
+
+    // Make an API request and graph it
+    processResponse : function(res) {
+        console.log(res);
+    if (!res.ok) {
+        throw new Error('Fitbit API request failed: ' + res);
+    }
+ 
+    var contentType = res.headers.get('content-type')
+    if (contentType && contentType.indexOf("application/json") !== -1) {
+        return res.json();
+    } else {
+        throw new Error('JSON expected but received ' + contentType);
+    }
+}
+
+
+
+
+};
+iceAge.init();
